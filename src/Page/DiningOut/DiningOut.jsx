27ac55cb94c5405fcc858d5caa52656d6626835e
Naresh-1308 <raghavgupta227@gmail.com>
2023-10-20 16:38:22 +0530

    naresh

--- src/Page/DiningOut/DiningOut.jsx
+++ src/Page/DiningOut/DiningOut.jsx
@@ -1,41 +1,10 @@
 import React from 'react';
 import "./DiningOut.scss";
-import Filters from '../../components/Filters/Filters';
-
-
-const diningFilters = [
-    {
-        id: 1,
-        icon: <i class="fi fi-rr-settings-sliders absolute-center"></i>,
-        title: "Filters",
-    },
-    {
-        id: 2,
-        title: "Rating 4.0+",
-    },
-    {
-        id: 3,
-        title: "Outdoor Seating",
-    },
-    {
-        id: 4,
-        title: "Alcohol was served",
-    },
-    {
-        id: 5,
-        title: "Still Oen",
-    },
-];
-
 
 const DiningOut = () => {
     return (
-        <div>
-            <div className="max-width">
-                <Filters filterList = {diningFilters} />
-            </div>
-        </div>
+        <div>DiningOut</div>
     );
-}
+};
 
 export default DiningOut;
